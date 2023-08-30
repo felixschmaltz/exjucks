867a7a871cc5c69879d5b14ef69829af1f38f656
Felix Schmaltz <schmaltz@m-creations.com>
2023-08-30 10:31:41 +0200

    Add table and css

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ nunjucks.configure('templates', {
 
 app.use('/static', express.static('static'));
 
+patientList = [
+  {
+    name: 'John Doe',
+    age: 55,
+    gender: 'male'
+  },
+  {
+    name: 'Peter Pan',
+    age: 11,
+    gender: 'male'
+  },
+  {
+    name: 'Lise Meitner',
+    age: 44,
+    gender: 'female'
+  }
+];
+
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index.html', {patients: patientList})
 })
 
 app.listen(port, () => {
