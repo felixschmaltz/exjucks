39f1c0ab576e68dea3cb1eea559a89dc74ff45f2
Felix Schmaltz <schmaltz@m-creations.com>
2023-09-01 15:59:32 +0200

    Show a hoverable patient list with age

--- app.js
+++ app.js
@@ -35,10 +35,44 @@ patientList = [
 app.get('/', (req, res) => {
   // console.log(req)
   fetchData('Patient')
-    .then(patientList => res.render('index.html', {patients: patientList}))
-    .catch(error => res.sendStatus(503))
+    .then(patientList => {
+      const pL = formatPatientlist(patientList);
+      res.render('index.html', {patients: pL});
+    })
+    .catch((err) => {
+      console.error(err)
+      res.sendStatus(503)
+    })
 })
+
+const formatPatientlist = (patientList) => {
+  return patientList.map((i) => {
+    return {
+      name: (i.name) ?  i.name[0].given[0].charAt(0) + ' ' + i.name[0].family: '',
+      age: patientAge(i),
+      gender: patientGender(i)
+    }
+  })
+}
+
+
+const patientName = (patient) => {
+  const officialName = jp.value(patient, '$.name[?(@.use=="official")]');
+  return `${officialName.given[0].charAt(0)}. ${officialName.family}`;
+};
+
+const patientAge = (patient) => {
+  if (patient.birthDate) {
+    return new Date(Date.now() - new Date(patient.birthDate)).getUTCFullYear() - 1970;
+  } else {
+    return 0;
+  }
+}
+
+const patientGender = (patient) => patient.gender;
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
